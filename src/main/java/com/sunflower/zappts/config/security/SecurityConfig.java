fec2bead62384eef0187ecc4122aaaed579c5801
Jackson Almeida <jacksonnalmeida30@gmail.com>
2022-07-04 20:34:45 -0400

    test prod

--- src/main/java/com/sunflower/zappts/config/security/SecurityConfig.java
+++ src/main/java/com/sunflower/zappts/config/security/SecurityConfig.java
@@ -67,9 +67,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.cors().and().csrf().disable();
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		http.authorizeHttpRequests()
-				.antMatchers(HttpMethod.POST, "/api/cards/**").permitAll()
-				.antMatchers(HttpMethod.POST, "/api/roles/newrole").permitAll()
-				.antMatchers(HttpMethod.POST, "/api/players/**").permitAll()
+				.antMatchers(HttpMethod.GET, "/api/cards/**").permitAll()
 				.antMatchers(HttpMethod.POST, "/api/auth/login").permitAll().anyRequest().authenticated().and()
 				.addFilterBefore(new AuthenticationTokenFilter(tokenService, playerRepository),
 						UsernamePasswordAuthenticationFilter.class);
